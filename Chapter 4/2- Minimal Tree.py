672e3ef747a8270306cf8bb14b818edf20b556ae
Raj <51259329+workingpayload@users.noreply.github.com>
2022-08-22 11:42:05 +0530

    Create 2- Minimal Tree.py

---
+++ Chapter 4/2- Minimal Tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if nums!=[]:
+            half = len(nums)//2
+            root = TreeNode(nums[half])
+            root.left = self.sortedArrayToBST(nums[:half])
+            root.right = self.sortedArrayToBST(nums[half+1:])
+            return root
+        else:
+            return None
+        
