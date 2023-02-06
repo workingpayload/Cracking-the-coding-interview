89c119e265105e7d72f12ab698ad426f2ee23b2c
Raj <51259329+workingpayload@users.noreply.github.com>
2023-02-06 14:16:03 +0530

    Update 9- String Rotation.py

--- Chapter 1/9- String Rotation.py
+++ Chapter 1/9- String Rotation.py
@@ -1,14 +1,14 @@
-def issubstring(s,t):
-    if s in t:
+def issubstring(s,ti):
+    if s in ti:
         return True
     else:
         return False
 
-def check(s,t):
+def check(s,ti):
     n = len(s)
 
-    if len(t)>0 and n>0:
+    if len(ti)>0 and n>0:
         new_s = s+s
-        return issubstring(t,new_s)
+        return issubstring(ti,new_s)
     else:
         return False
